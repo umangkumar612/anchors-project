43607457fac6585f772bf3f7815b4d8d8509c8ea
umang___kataria__ <umangkumarravi123@gmail.com>
2026-05-26 23:21:09 +0530

    fix preview allowed hosts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,8 +3,15 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+
   server: {
     host: '0.0.0.0',
+    allowedHosts: true
+  },
+
+  preview: {
+    host: '0.0.0.0',
+    port: 3000,
     allowedHosts: true
   }
 })
